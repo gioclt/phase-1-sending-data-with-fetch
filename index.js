212849145d0c1c0f5eab39c012344eaa6e328fb2
Yobany Caltempa <gioclt26@gmail.com>
2022-03-30 16:39:56 -0400

    commit tree faa75ed17e8ae974260a090e0c670234520d11b4

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+
+function submitData(name, email) {
+    const configObject = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": name,
+            "email": email
+        })
+    };
+    return fetch("http://localhost:3000/users", configObject)
+        .then(response => response.json())
+        .then(body => addNewContact(body.id))
+        .catch(function (error) {
+            alert("Unauthorized Access");
+            document.body.innerHTML=error.message
+        })
+    
+}
+
+function addNewContact(newId) {
+    document.getElementsByTagName("body")[0].innerHTML = newId;
+}
+
+submitData("name", "name@name.com")
